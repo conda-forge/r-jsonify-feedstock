504f27edd7204b055dd56faa0607d9fea6149bb6
Devon Ryan <dpryan79@users.noreply.github.com>
2022-10-13 10:25:27 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -o errexit -o pipefail
+export PKG_CPPFLAGS="-DHAVE_WORKING_LOG1P"
 if [[ ${target_platform} =~ linux.* ]] || [[ ${target_platform} == win-32 ]] || [[ ${target_platform} == win-64 ]] || [[ ${target_platform} == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
   ${R} CMD INSTALL --build .
